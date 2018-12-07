47ac82942edd4c4a713f4166d12e188282e5c51a
BASIT KHAN <basit@BASITs-MacBook-Pro.local>
2018-12-08 01:36:03 +0400

    Test

--- Reach.Repository/Repository/TuteeRepository.cs
+++ Reach.Repository/Repository/TuteeRepository.cs
@@ -7,6 +7,7 @@ namespace Reach.Repository.Repository
     public class TuteeRepository : Repository<Tutee> , IRepository<Tutee>
     {
         public IEnumerable<Tutee> DoSomeOtherTask(){
+            // Do Something
             throw new Exception("No implementation done by Reach");
         }
     }
